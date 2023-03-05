453edba2b078867c53761684c58ea0e6f9a94cba
SoyJeroMe <ojeronimo360@gmail.com>
2023-03-05 18:20:21 -0500

    agregar estilos a la pagina de productos

--- src/autos/pages/ProductPage.jsx
+++ src/autos/pages/ProductPage.jsx
@@ -8,6 +8,7 @@ import { GiSteeringWheel } from 'react-icons/gi'
 import { MdOutlineFlipCameraAndroid, MdTouchApp } from 'react-icons/md'
 import { TbGps } from 'react-icons/tb'
 import Footer from '../../ui/components/Footer'
+import '../../styles/ProductPage.css'
 
 const ProductPage = () => {
 
@@ -24,90 +25,93 @@ const ProductPage = () => {
         <>
             <Header />
             <div>
-                <section>
+                <section className='title-info'>
                     <div>
                         <p>{`Auto ${auto.category}`}</p>
-                        <h3>{auto.title}</h3>
+                        <h3 className='auto-title'>{auto.title}</h3>
                     </div>
-                    <button
+                    <button className='return-button'
                         onClick={onNavigateBack}
                     >
                         Volver
                     </button>
                 </section>
-                <section>
-                    <div>
-                        <GoLocation />
+                <section className='location-info'>
+                    <div className='location'>
+                        <GoLocation className='location-icon'/>
                         <p><b>{auto.location}</b>, Argentina</p>
                     </div>
-                    <p>A 940 metros del centro</p>
+                    <p className='distance'>A 940 metros del centro</p>
                 </section>
-                <section>
-                    <div>
-                        <img src={auto.img} alt={auto.title} />
+                <section className='img-auto'>
+                    <div className='images-group'>
+                        <img className='image' src={auto.img} alt={auto.title} />
                         <img src={auto.img} alt={auto.title} />
                         <img src={auto.img} alt={auto.title} />
                         <img src={auto.img} alt={auto.title} />
                         <img src={auto.img} alt={auto.title} />
                     </div>
-                    <button> Ver Mas </button>
+                    <button className='view-more-button'> Ver Mas </button>
                 </section>
-                <section>
-                    <h3>Disfruta de tu viaje</h3>
+                <section className='description'>
+                    <h3 className='description-message'>Disfruta de tu viaje</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quisquam quasi impedit sit voluptas a. Ipsa vitae facilis, accusantium consequatur quisquam doloribus, nostrum ab minus mollitia libero nulla voluptas sint!</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea rem atque recusandae illum porro perferendis voluptas quisquam aspernatur accusamus omnis? Quod ab unde eius ut ipsa eligendi magni, dolores nobis!</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae nemo odio vel rem voluptatem animi error est sint ullam ipsum laudantium, quia, ratione culpa impedit. Temporibus praesentium dolorem sit ipsam?</p>
                 </section>
-                <section>
-                    <h3>¿Que ofrece este auto?</h3>
-                    <div>
+
+                <section className='details-section'>
+                    <h3 className='details-title'>¿Que ofrece este auto?</h3>
+                    <hr/>
+                    <div className='details'>
                         <div>
-                            <BsSnow />
+                            <BsSnow className='icon'/>
                             <p>Aire acondicionado</p>
                         </div>
                         <div>
-                            <BsThermometerSun />
+                            <BsThermometerSun className='icon'/>
                             <p>Calentador de asiento</p>
                         </div>
                         <div>
-                            <GiSteeringWheel />
+                            <GiSteeringWheel className='icon'/>
                             <p>Piloto automatico</p>
                         </div>
                         <div>
-                            <MdOutlineFlipCameraAndroid />
+                            <MdOutlineFlipCameraAndroid className='icon'/>
                             <p>Camara retrovisora</p>
                         </div>
                         <div>
-                            <BsFillPersonCheckFill />
+                            <BsFillPersonCheckFill className='icon'/>
                             <p>Asistente</p>
                         </div>
                         <div>
-                            <MdTouchApp />
+                            <MdTouchApp className='icon'/>
                             <p>Pantalla tactil</p>
                         </div>
                         <div>
-                            <TbGps />
+                            <TbGps className='icon'/>
                             <p>GPS</p>
                         </div>
                     </div>
-                </section>
-                <section>
-                    <h3>Que tenes que saber</h3>
-                    <div>
-                        <div>
-                            <h4>Normas del auto</h4>
+                </section >
+                <section className='rules-and-recommendations'>
+                    <h3 className='rules-and-recommendations-title'>Qué tenés que saber</h3>
+                    <hr />
+                    <div className='sections'>
+                        <div className='section-info'>
+                            <h4 className='section-title'>Normas del auto</h4>
                             <p>Devolver con tanque lleno</p>
                             <p>No fumar</p>
                             <p>Cantidad maxima de personas segun cantidad de asientos</p>
                         </div>
                         <div>
-                            <h4>Salud y seguridad</h4>
+                            <h4 className='section-title'>Salud y seguridad</h4>
                             <p>Contar con seguro basico</p>
                             <p>Menores de 12 años no pueden ir adelante</p>
                             <p>Deposito de seguridad</p>
                         </div>
                         <div>
-                            <h4>Politicas de cancelacion</h4>
+                            <h4 className='section-title'>Politicas de cancelacion</h4>
                             <p>Agrega las fechas de tu viaje para obtener los detalles de cancelacion de este auto</p>
                         </div>
                     </div>
