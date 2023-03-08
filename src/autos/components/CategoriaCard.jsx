1a933f96440ad67abd76a70f07bd52a452c0af43
Bautista <bautiluciani@hotmail.com>
2023-03-08 17:02:45 -0300

    Mejorando cantidad de autos

--- src/autos/components/CategoriaCard.jsx
+++ src/autos/components/CategoriaCard.jsx
@@ -1,29 +1,7 @@
 import React from 'react'
 import '../../styles/CategoriaCard.css'
 
-const CategoriaCard = ({ titulo, imagen }) => {
-
-  let cantidadEconomico = 0;
-  let cantidadDeLujo = 0;
-  let cantidadCamioneta = 0;
-  let cantidadDeportivo = 0;
-
-  switch (titulo) {
-    case "Economico":
-      cantidadEconomico += 1
-      break;
-    case "Deportivo":
-      cantidadDeportivo += 1
-      break;
-    case "De Lujo":
-      cantidadDeLujo += 1
-      break;
-    case "Camioneta":
-      cantidadCamioneta += 1
-      break;
-    default:
-      break;
-  }
+const CategoriaCard = ({ titulo, imagen, cantidad }) => {
 
   return (
     <div>
@@ -34,13 +12,13 @@ const CategoriaCard = ({ titulo, imagen }) => {
         <h3>{titulo}</h3>
         {
           (titulo == "Economico")
-          ? <p>{cantidadEconomico} disponible(s)</p>
+          ? <p>{cantidad[0]} disponible(s)</p>
           : (titulo == "Deportivo")
-          ? <p>{cantidadDeportivo} disponible(s)</p>
+          ? <p>{cantidad[1]} disponible(s)</p>
           : (titulo == "De Lujo")
-          ? <p>{cantidadDeLujo} disponible(s)</p>
+          ? <p>{cantidad[2]} disponible(s)</p>
           : (titulo == "Camioneta")
-          ? <p>{cantidadCamioneta} disponible(s)</p>
+          ? <p>{cantidad[3]} disponible(s)</p>
           : <p> 0 disponible(s)</p>
         }
       </div>
--- src/autos/components/CategoriaList.jsx
+++ src/autos/components/CategoriaList.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import CategoriaCard from './CategoriaCard'
 import '../../styles/CategoriaList.css'
 import useFetchCategorias from '../../hooks/useFetchCategorias'
+import useFetchProductos from '../../hooks/useFetchProductos'
 
 const CategoriaList = () => {
 
   const { isLoading, categorias } = useFetchCategorias()
+  const { productos } = useFetchProductos()
+  
+  const cantidad = ()=> {
+    let cantidadEconomico = 0;
+    let cantidadDeLujo = 0;
+    let cantidadCamioneta = 0;
+    let cantidadDeportivo = 0;
+
+    productos.map(prod => {
+      switch (prod.categoria.titulo) {
+        case "Economico":
+          cantidadEconomico += 1
+          break;
+        case "Deportivo":
+          cantidadDeportivo += 1
+          break;
+        case "De Lujo":
+          cantidadDeLujo += 1
+          break;
+        case "Camioneta":
+          cantidadCamioneta += 1
+          break;
+        default:
+          break;
+      }})
+
+    return [cantidadEconomico, cantidadDeportivo, cantidadDeLujo, cantidadCamioneta]
+  }
+  
 
   return (
     <>
@@ -14,7 +44,11 @@ const CategoriaList = () => {
       }
       <div className="categorys">
         {categorias.map((cate) => (
-          <CategoriaCard key={cate.id} {...cate} />
+          <CategoriaCard
+            key={cate.id}
+            {...cate}
+            cantidad = {cantidad()}
+          />
         ))}
       </div>
     </>
