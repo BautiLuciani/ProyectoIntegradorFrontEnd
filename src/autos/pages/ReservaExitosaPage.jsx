e2770310ee1f41050456d3313acaa07f002d2bf0
SoyJeroMe <ojeronimo360@gmail.com>
2023-04-03 19:09:48 -0500

    agregar boceto de pagina de reserva exitosa

--- src/autos/pages/ReservaExitosaPage.jsx
+++ src/autos/pages/ReservaExitosaPage.jsx
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom'
 import Footer from '../../ui/components/Footer'
 import Header from '../../ui/components/Header'
 import { GoVerified } from 'react-icons/go'
+import '../../styles/ReservaExitosa.css'
+
 
 const ReservaExitosaPage = () => {
   return (
     <>
         <Header/>
-        <div>
-            <GoVerified/>
+        <div className='booking-confirmed'>
+            <GoVerified className='icon-verified'/>
             <p><b>¡Muchas gracias!</b></p>
             <p>Su reserva se ha realizado con exito</p>
-            <Link to={`/home`}>
+            <Link className='link-to-home' to={`/home`}>
                 ok
             </Link>
         </div>
